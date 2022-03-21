3db8541d4b0c71aa0e6139dfcc67b2578a4f4f83
PlexTDM <tengisteka0631@gmail.com>
2022-03-21 19:31:47 +0800

    svg logo to png logo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
@@ -9,11 +9,13 @@ import store from './components/store';
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <HashRouter>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
+      </HashRouter>
     </Provider>
   </StrictMode>,
   document.getElementById('root')
