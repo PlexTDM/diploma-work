c3f2b6b157924d86c81c8ebd1a66469be3a39b69
PlexTDM <tengisteka0631@gmail.com>
2022-04-22 23:24:41 +0800

    api

--- src/components/actions/authActions.js
+++ src/components/actions/authActions.js
@@ -1,8 +1,8 @@
 import * as constants from "../constants/constants";
 import axios from "axios";
 
-const uri = "http://localhost:4000";
-// const uri = '';
+// const uri = "http://localhost:4000";
+const uri = '';
 
 export const registerUsers = (formData) => async (dispatch) => {
   dispatch({
